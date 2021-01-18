da68b2edf42a8bd7597acace5c60114fd25a8d1f
Rizal <rizalfadilah.rf@gmail.com>
2021-01-18 17:56:05 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,3 +9,18 @@ function multiplesOf3and5(number) {
   }
   return sum;
 }
+
+// Problem 2
+// Considering the terms in the Fibonacci sequence whose values do not exceed n, find the sum of the even-valued terms.
+
+function fiboEvenSum(n) {
+  let sum = 0, i = 1, fiboNum = [0, 1];
+  do{
+    i++;
+    fiboNum[i] = fiboNum[i-1] + fiboNum[i-2];
+    if(fiboNum[i]%2 == 0){
+      sum += fiboNum[i];
+    }
+  }while(fiboNum[i] < n);
+  return sum;
+}
